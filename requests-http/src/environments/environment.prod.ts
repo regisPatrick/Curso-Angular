e548204d92e983cab544f7957d46ce634dab921a
regisPatrick <regis86@hotmail.com>
2020-09-03 22:31:53 -0300

    commit 03-09-2020

---
+++ requests-http/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// export const environment = {
+//   production: true,
+//   API: 'http://localhost:3000/'
+// };
+
+export const environment = {
+  production: true,
+  API: '/',
+  BASE_URL: ''
+};
